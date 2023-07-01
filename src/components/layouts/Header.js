fc55c85cbf65d43db1f9968d77081bb6a9bd7ae8
kushal1997 <kushalrao103@gmail.com>
2023-07-01 18:01:59 +0530

    changed header

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -4,12 +4,28 @@ import "./header.css"
 export const Header = () => {
 
     return (
-        <header>
+        <>
             <Sidebar />
 
             <div className="container">
-                <h2>Interview Question & Answers</h2>
+                <svg viewBox="0 0 800 200">
+                    <symbol id="s-text">
+                        <text textAnchor="middle" x="50%" y="80%" >
+                            INTERVIEW
+                        </text>
+                    </symbol>
+                    <g className="g-ants">
+                        <use xlinkHref="#s-text" className="text-copy" />
+                        <use xlinkHref="#s-text" className="text-copy" />
+                        <use xlinkHref="#s-text" className="text-copy" />
+                        <use xlinkHref="#s-text" className="text-copy" />
+                        <use xlinkHref="#s-text" className="text-copy" />
+                    </g>
+                </svg>
+
+                
             </div>
-        </header>
+
+        </>
     )
 }
