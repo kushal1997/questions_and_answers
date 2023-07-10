d8710cf753bd96733eb981d8254025088f16d770
kushal1997 <kushalrao103@gmail.com>
2023-07-10 16:41:20 +0530

    restore previous files

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import { Link, NavLink } from 'react-router-dom';
-import $ from "jquery"
+import { NavLink } from 'react-router-dom';
 
 export const Sidebar = () => {
     const [reactRotated, setreactRotated] = useState(false);
