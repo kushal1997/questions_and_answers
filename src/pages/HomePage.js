cf20343eea562ce8240553a35838681c19d5c2a9
Monalisa-Panda18 <monalisapanda411@gmail.com>
2023-07-10 16:07:55 +0530

    small changes in css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,10 +4,10 @@ export const HomePage = () => {
   document.title=`InterView Questions`;
   return (
     <div className=" mt-11 p-1  sm:ml-64 rounded-md">
-      <div className="p-4 ml-10 bg-white dark:bg-[#030321]">
+      <div className="p-4 ml-10 bg-[#030321]">
 
         <main class="p-5 bg-light-blue">
-          <h1 class=" text-3xl font-semibold text-gray-800 lg:text-4xl dark:text-white">
+          <h1 class=" text-3xl font-semibold lg:text-4xl text-white">
             Home Page
           </h1>
 
